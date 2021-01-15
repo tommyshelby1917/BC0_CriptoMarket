a7fafbf20f93f8adf854cbaf61e2794fe577f003
Josep Miquel <tommyshelby@disroot.org>
2021-01-15 01:18:05 +0100

    BD conectada y tabla movimientos ok

--- criptomarket/forms.py
+++ criptomarket/forms.py
@@ -7,15 +7,7 @@ import sqlite3
 
 DBFILE = app.config['DBFILE']
 
-
-# class RegisterForm(FlaskForm):
-#     email_registre = EmailField('Correu electrònic', [validators.DataRequired()])
-#     password_registre = PasswordField('Contrasenya', [
-#         validators.DataRequired(), validators.EqualTo('confirm_registre', message='Les contrasenyes han de coincidir')
-#         ])
-#     confirm_registre = PasswordField('Repeteix la contrasenya')
-
 class CompraForm(FlaskForm):
-    moneda_compra_from = SelectField('En quina moneda vols pagar?', choices=['ETH','LTC','BNB','EOS','XLM','TRX','BTC','XRP','BCH','USDT','BSV','ADA'])
+    moneda_compra_from = SelectField('En quina moneda vols pagar?', choices=['EUR','ETH','LTC','BNB','EOS','XLM','TRX','BTC','XRP','BCH','USDT','BSV','ADA'])
     moneda_compra_to = SelectField('Quina moneda vols comprar?', choices=['ETH','LTC','BNB','EOS','XLM','TRX','BTC','XRP','BCH','USDT','BSV','ADA'])
-    cantidad_compra = FloatField('Quina cuantitat en €?', [validators.DataRequired(), validators.Length(min=1, max=99999)])
+    cantidad_compra = FloatField('Quina cuantitat en €?', [validators.DataRequired()])
--- criptomarket/views.py
+++ criptomarket/views.py
@@ -2,7 +2,7 @@ from criptomarket import app
 from criptomarket.forms import *
 from flask import render_template, request, url_for, redirect, flash, Response
 import sqlite3
-from datetime import date
+from datetime import date, time, datetime
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
@@ -11,40 +11,35 @@ import json
 DBFILE = app.config['DBFILE']
 API_KEY = app.config['API_KEY']
 
+def consultaApi(fromMoneda, toMoneda, cantidad=1):
 # Conexion con la API y parámetros
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-parameters = {
-'convert':'EUR',
-'symbol':'ETH,LTC,BNB,EOS,XLM,TRX,BTC,XRP,BCH,USDT,BSV,ADA'
-}
-headers = {
-'Accepts': 'application/json',
-'X-CMC_PRO_API_KEY': API_KEY,
-}
+    url = f"https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount={cantidad}&symbol={fromMoneda}&convert={toMoneda}&CMC_PRO_API_KEY={API_KEY}"
+    headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': API_KEY,
+    }
 
-session = Session()
-session.headers.update(headers)
+    session = Session()
+    session.headers.update(headers)
 
-# Recolecta datos JSON y Control errores
-try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
+    # Recolecta datos JSON y Control errores
+    try:
+        response = session.get(url, params=None)
+        data = json.loads(response.text)
+        
+    except (ConnectionError, Timeout, TooManyRedirects) as errores:
+        print(errores)
     
-except (ConnectionError, Timeout, TooManyRedirects) as errores:
-    print(errores)
+    valor_from = round(data['data']['amount'], 10)
+    valor_to = round(data['data']['quote'][toMoneda]['price'], 10)
+    
+    return valor_from, valor_to
 
-# Creamos diccionario con lista de monedas y su valor en euros
-lista_monedas = ['ETH','LTC','BNB','EOS','XLM','TRX','BTC','XRP','BCH','USDT','BSV','ADA']
-precio_monedas = []
-
-for moneda in lista_monedas:
-    precio_monedas.append(round(data['data'][f"{moneda}"]['quote']['EUR']['price'], 2))
-
-dict_monedas = dict(zip(lista_monedas,precio_monedas))
-print(dict_monedas)
+print(consultaApi('EUR', 'BTC'))
 
 # Función de la query a la BD
 def consulta(query, params=()):
+
     conn = sqlite3.connect(DBFILE)
     c = conn.cursor()
 
@@ -52,13 +47,43 @@ def consulta(query, params=()):
     conn.commit()
 
     filas = c.fetchall()
-
     conn.close()
+
+    if len(filas) == 0:
+        return filas
+    else:
+        columnNames = []
+        for columnName in c.description:
+            columnNames.append(columnName[0])
+
+        listaDeDiccionarios = []
+
+        for fila in filas:
+            d = {}
+            for ix, columnName in enumerate(columnNames):
+                d[columnName] = fila[ix]
+            listaDeDiccionarios.append(d)
+
+        return listaDeDiccionarios
+
 
 # Renderizado home
 @app.route('/')
 def index():
-    return render_template('index.html', dict_monedas=dict_monedas)
+
+    # Llista de monedes i valors a la home
+    lista_monedas = ['ETH','LTC','BNB','EOS','XLM','TRX','BTC','XRP','BCH','USDT','BSV','ADA']
+    valor_todas = []
+
+    lista_movimientos = consulta('SELECT * FROM movements;')
+    print(lista_movimientos)
+
+    for moneda in lista_monedas:
+        valor_todas.append(consultaApi('EUR', moneda))
+    
+    dict_monedas = dict(zip(lista_monedas,valor_todas))
+
+    return render_template('index.html', dict_monedas=dict_monedas, lista_movimientos=lista_movimientos)
 
 # Renderizado registro
 @app.route('/purchase', methods=['GET', 'POST'])
@@ -71,13 +96,21 @@ def purchase():
         print("El methodo ha sido post!")
         if form.validate():
             print("Se ha validado!")
-            # consulta('INSERT INTO users (password, email, fecha_alta) VALUES (?, ? ,? );', 
-            #         (
-            #             form.password_registre.data,
-            #             form.email_registre.data,
-            #             date.today(),
-            #         )
-            # )
+            
+            total_compra = consultaApi(form.moneda_compra_from.data, form.moneda_compra_to.data, form.cantidad_compra.data)
+
+            print(float(total_compra[1]))
+
+            consulta('INSERT INTO MOVEMENTS (date, time, from_moneda, from_cantidad, to_moneda, to_cantidad) VALUES (?, ?, ?, ?, ?, ? );', 
+                    (
+                        str(date.today()),
+                        str(datetime.now().time()),
+                        str(form.moneda_compra_from.data),
+                        float(form.cantidad_compra.data),
+                        str(form.moneda_compra_to.data),
+                        float(total_compra[1])
+                    )
+            )
 
             return redirect(url_for('purchase'))
         else:
